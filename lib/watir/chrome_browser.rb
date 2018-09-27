167676cb1ae07898774930042560174aaafa6762
liontwinkle <lion.twinkle.ge@gmail.com>
2018-09-27 23:16:24 +0300

    Fix for watir on heroku

--- lib/watir/chrome_browser.rb
+++ lib/watir/chrome_browser.rb
@@ -1,7 +1,13 @@
 module Watir
   class ChromeBrowser
     def self.in_browser
-      browser = Watir::Browser.new :chrome, headless: true
+      browser_options = { headless: true }
+
+      # set chrome path for heroku
+      if (chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil))
+        browser_options[:options] = { binary: chrome_bin }
+      end
+      browser = Watir::Browser.new :chrome, browser_options
       begin
         yield browser
       ensure
