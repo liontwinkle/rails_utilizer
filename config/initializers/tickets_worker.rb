5e2b1209f8fe9f0203276abe46783b295d227e8c
liontwinkle <lion.twinkle.ge@gmail.com>
2018-09-27 22:16:07 +0300

    Only initialize job if delayed jobs present

--- config/initializers/tickets_worker.rb
+++ config/initializers/tickets_worker.rb
@@ -1,4 +1,4 @@
 # Cron or HerokuScheduler should be used for it.
 # But HerokuScheduler seems to not be supported on free dynos
 # So, doing a bit of a hack - init a job, which would reenque itself on success
-NewTicketsWorker.new.enqueue_self
+NewTicketsWorker.new.enqueue_self if ActiveRecord::Base.connection.table_exists? 'delayed_jobs'
