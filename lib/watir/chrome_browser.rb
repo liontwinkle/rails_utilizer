a07bbd272f07b73e19b5b42a6a2fe2b04a4a5777
liontwinkle <lion.twinkle.ge@gmail.com>
2018-09-27 23:42:38 +0300

    Trying with additional watir options

--- lib/watir/chrome_browser.rb
+++ lib/watir/chrome_browser.rb
@@ -7,6 +7,7 @@ module Watir
       if (chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil))
         browser_options[:options] = { binary: chrome_bin }
       end
+      browser_options[:switches] = %w[--disable-dev-shm-usage --no-sandbox --ignore-certificate-errors --disable-popup-blocking --disable-translate --disable-notifications --start-maximized --disable-gpu --headless]
       browser = Watir::Browser.new :chrome, browser_options
       begin
         yield browser
