d484f804c54042b063d22d36f0710b31a1d3131d
liontwinkle <lion.twinkle.ge@gmail.com>
2018-12-14 18:04:25 +0200

    [WIP] Use Api calls to get seats info

--- app/logic/train_searches/check_tickets_logic.rb
+++ app/logic/train_searches/check_tickets_logic.rb
@@ -3,17 +3,21 @@ module TrainSearches
     include Singleton
 
     def search
-      Watir::ChromeBrowser.in_browser do |browser|
-        fulfill_train_searches(browser)
-      end
-    end
-
-    private
-
-    def fulfill_train_searches(browser)
       TrainSearch.unfulfilled.find_each do |train_search|
         FulfillTrainSearchLogic.new(train_search, browser).fulfill
       end
+
+      # options = {
+      #   body: {
+      #     date: '2019-01-18',
+      #     from: 22_00_001,
+      #     to: 2_210_700,
+      #     time: '00:00'
+      #   }
+      # }
+      #
+      # response = HTTParty.post('https://booking.uz.gov.ua/en/train_search/', options)
+      # response
     end
   end
 end
